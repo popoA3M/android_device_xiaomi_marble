831967cd5f61bc8caf7b7766f40785a3bbd8547f
popoA3M <pratyayaborborah@gmail.com>
2023-10-18 08:34:26 +0200

    marble: PixelOS bringup
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,17 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := aosp_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
