ff4edb9ccb59031699ee15af7096a2d9ec843a22
saikiran2001 <bjsaikiran@gmail.com>
2023-12-28 06:47:37 +0000

    marble: Enable some GMS related flags
    * Include ARCORE
    * Include Live wallpapers
    * Include NGA
    * Include Google Recorder
    * Include Youtube
    * Also set bootanimation resolution

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -10,9 +10,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+# GMS Flags
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_YOUTUBE := true
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
